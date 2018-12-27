0d938be6e242a92b4f60548cce5d05c637e85714
Kurt Cooney <krtcotmo2@gmail.com>
2018-12-26 21:57:49 -0500

    added comment

--- assets/scripts/gifPick.js
+++ assets/scripts/gifPick.js
@@ -2,44 +2,49 @@ let peeps = [`Gary Oldman`, `Casey Affleck`, `Leonardo DiCaprio`, `Eddie Redmayn
 let btnList = [];
 let gifSetter = {
 	getGifs: function (arg) {
+		//URL for gif - paramter for query at end
 		arg = arg.replace(" ", "+");
 		var queryURL = `https://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&limit=10&q=${arg}`;
+		
+		//AJAX CALL
 		$.ajax({
 			url: queryURL,
 			method: "GET"
 		}).then(function (response) {
+			//repalce a + with space in teh event the URL has a + in it - adds to teh screen as a header for the section
 			let grpHolder = $("<div>").append(`<h5>${arg.replace("+", " ")}</h5>`);
 			response.data.forEach(function (o) {
+				//creates image, sets class name and sets the ani and still data values with urls of the two images
 				let img = $("<img>").attr("data-ani", o.images.fixed_height_small.url)
+				.attr("class", "aGif")
 				.attr("data-playing", "false")
 				.attr("data-still", o.images.fixed_height_small_still.url)
-				.attr("src", o.images.fixed_height_small_still.url)
-				.attr("class", "aGif");
+				.attr("src", o.images.fixed_height_small_still.url);
+				//adds gif to the holder
 				grpHolder.append(img)
 				$(".gifHolder").prepend(grpHolder);
 			});
 		}).catch(function (response) {
+			//error if the giphy api fails
 			$(".modal-body").html("There was an error attempting to get information from Giphy.com.");
 			$('#theModal').modal();
 		});
 	},
 	offerOptions: function (arg) {
+		//if more than one option matches search criteria add instructions
 		$(".modal-body").html("")
 		let inst = $("<p>").text(`Several options could match your request. Click one of the buttons below to add the actor/actress or click ok to clsoe the options.`);		
 		$(".modal-body").append(inst);
+		//loop though the optoions and create abutton for each
 		for(let d =0; d<arg.length;d++){			
 			let btn = $(`<button class='oneOfMany'>${arg[d]}</button>`);
 			$(".modal-body").append(btn);
 		}
-		//let instance = this;
-		// $(".oneOfMany").on("click", function(){
-		// 	instance.checkName(this.innerText.toLowerCase(), true, instance.getGifs);
-		// 	peeps = peeps.filter( x => x != this.innerText)
-		// 	$('#theModal').modal('hide');
-		// })
+		//open modal
 		$('#theModal').modal();
 	},
 	addButton: function (nameOnbtn) {
+		//creates a button for the check name and game start function
 		let btn = $(`<button class='btnActor'>${nameOnbtn}</button>`);
 		btn.attr("data-name", nameOnbtn);		
 		btnList.push(nameOnbtn);
@@ -48,37 +53,46 @@ let gifSetter = {
 	checkName: function (theName, match,  callback) {
 		let potentail = [];
 		peeps.forEach(function (o) {
+			//loops thorugh the peeps array and looks for any string that contains the search criteria - match boolean is a trigger to prevent from running
 			if (o.toLowerCase().includes(theName) && !match) {
 				potentail.push(o);
 				person = o;
 			}
+			//loops thorugh the peeps array and looks for the exact match for the search criteria - match boolean is a trigger to prevent from running
 			if (o.toLowerCase() == theName && match) {
 				potentail.push(o);
 				person = o;
 			}
 		});
 		if (potentail.length == 0) {
+			//handle if there is no response
 			$(".modal-body").html("")
 			let inst = $("<p>").text(`No one with a name similar to ${$("#tboxActor").val().trim()} has won an Oscar`);		
 			$(".modal-body").append(inst);
 			$('#theModal').modal();
 			return;
 		} else if (potentail.length > 1) {
+			//handle response if there is more than 1 search result
 			this.offerOptions(potentail);
 			return;
 		}
+		
+		//check to see if the option we are searching for already exists in our list
 		if (btnList.indexOf(potentail[0]) > -1) {
+			//person exists
 			$(".modal-body").html("")
 			let inst = $("<p>").text(`Results for ${$("#tboxActor").val().trim()} are already in your list`);		
 			$(".modal-body").append(inst);
 			$('#theModal').modal();
 		} else {
+			//person does not exist
 			callback(potentail[0]);
 			$(".btnHolder").append(this.addButton(potentail[0]));
 			$("#tboxActor").val("");
 		}
 	},
 	startBtnList: function () {
+		//add 10 buttons from the aray of oscar winners
 		for (let d = 0; d < 10; d++) {
 			let nameIndex = Math.floor(Math.random() * peeps.length);
 			let btn = this.addButton(peeps[nameIndex]);
